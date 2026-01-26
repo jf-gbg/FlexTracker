f231cf65638a3e87af4bfedb56837faf515d690f
jf-gbg <66627787+jf-gbg@users.noreply.github.com>
2026-01-26 20:48:22 +0100

    Lägga till TimeEntryDbo.cs

---
+++ src/backend/FlexTracker.Infrastructure/Models/TimeEntryDbo.cs
@@ -0,0 +1,11 @@
+﻿namespace FlexTracker.Infrastructure.Models;
+
+public class TimeEntryDbo
+{
+    public int Id { get; set; }
+    public DateOnly Date { get; set; }
+    public TimeOnly StartTime { get; set; }
+    public TimeOnly EndTime { get; set; }
+    public TimeOnly? LunchStartTime { get; set; }
+    public TimeOnly? LunchEndTime { get; set; }
+}
