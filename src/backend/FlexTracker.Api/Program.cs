072317ff676b25752ae4e5308bec2399470e2dcb
JoshuaFranklin <joshua.franklin@gotit.se>
2026-02-02 08:52:22 +0100

    Add project reference and configure DbContext for SQLite

--- src/backend/FlexTracker.Api/Program.cs
+++ src/backend/FlexTracker.Api/Program.cs
@@ -1,8 +1,16 @@
+using FlexTracker.Infrastructure.Persistence;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
+builder.Services.AddDbContext<AppDbContext>(options =>
+{
+    var connectionString = builder.Configuration.GetConnectionString("Default");
+    options.UseSqlite(connectionString);
+});
 
 var app = builder.Build();
 
