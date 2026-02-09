3cfce4bc9d70e2ceaebfef9bf94ea71215443b82
JoshuaFranklin <joshua.franklin@gotit.se>
2026-02-09 11:20:01 +0100

    Recreate migrations

--- src/backend/FlexTracker.Infrastructure/Migrations/20260209101123_InitialCreate.Designer.cs
+++ src/backend/FlexTracker.Infrastructure/Migrations/20260209101123_InitialCreate.Designer.cs
@@ -11,14 +11,14 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace FlexTracker.Infrastructure.Migrations
 {
     [DbContext(typeof(AppDbContext))]
-    [Migration("20260202081627_InitialCreate")]
+    [Migration("20260209101123_InitialCreate")]
     partial class InitialCreate
     {
         /// <inheritdoc />
         protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
-            modelBuilder.HasAnnotation("ProductVersion", "10.0.2");
+            modelBuilder.HasAnnotation("ProductVersion", "9.0.2");
 
             modelBuilder.Entity("FlexTracker.Infrastructure.Models.TimeEntryDbo", b =>
                 {
--- src/backend/FlexTracker.Infrastructure/Migrations/AppDbContextModelSnapshot.cs
+++ src/backend/FlexTracker.Infrastructure/Migrations/AppDbContextModelSnapshot.cs
@@ -15,7 +15,7 @@ namespace FlexTracker.Infrastructure.Migrations
         protected override void BuildModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
-            modelBuilder.HasAnnotation("ProductVersion", "10.0.2");
+            modelBuilder.HasAnnotation("ProductVersion", "9.0.2");
 
             modelBuilder.Entity("FlexTracker.Infrastructure.Models.TimeEntryDbo", b =>
                 {
